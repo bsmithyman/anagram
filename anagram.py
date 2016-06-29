dc1ff2cad379540c82ff1d9e0231645350568cee
Brendan Smithyman <brendan@3ptscience.com>
2016-06-29 00:20:41 -0600

    Initial commit.

---
+++ anagram.py
@@ -0,0 +1,140 @@
+"""anagram.py: the wrong way to solve a problem"""
+
+import collections
+import functools
+import imp
+import os
+import sys
+
+
+def chunk(lst, n):
+    """Iterate over n chunks"""
+    chunksize = len(lst) // n
+    for i in range(n):
+        yield lst[i*chunksize:(i+1)*chunksize] if i < (n - 1) else lst[i*chunksize:]
+
+def track(lst):
+    """Return a Tracker initialized with the list"""
+    tracker = Tracker()
+    tracker(lst)
+    return tracker
+
+
+class Anagram(object):
+    """Implements parsing of anagrams"""
+
+    def __init__(self, word):
+        self.word = word
+
+    @property
+    def baseform(self):
+        """The word's base form"""
+        return ''.join(sorted(self.word.lower()))
+
+    def __hash__(self):
+        """Implement hash collisions for anagrams"""
+        return hash(self.baseform)
+
+    def __str__(self):
+        """Return the string representation"""
+        return '{name}(\'{word}\') --> \'{baseform}\''.format(
+            name=self.__class__.__name__,
+            word=self.word,
+            baseform=self.baseform,
+            )
+
+    def __repr__(self):
+        """Return an evaluable representation"""
+        return '{name}("""{word}""")'.format(
+            name=self.__class__.__name__,
+            word=self.word,
+            )
+
+
+class Originals(set):
+    """Tracker to index originals"""
+
+    def __call__(self, word):
+        """Track and pass original word"""
+        self.add(word)
+        return word.baseform
+
+    def __add__(self, other):
+        """Implement 'self + other'"""
+        instance = Originals(self)
+        instance.update(other)
+        return instance
+
+    def __radd__(self, other):
+        """Implement 'other + self'"""
+        instance = Originals(other)
+        instance.update(self)
+        return instance
+
+
+class Tracker(collections.Counter):
+    """Tracker to collect anagrams"""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.originals = {}
+
+    def __call__(self, words):
+        """Intake words"""
+        self.update((self.originals.setdefault(pa.baseform, Originals())(pa) \
+            for pa in (Anagram(word) for word in words)))
+
+    def __add__(self, other):
+        """Implement 'self + other'"""
+        result = super().__add__(other)
+        result.originals = {key: self.originals.get(key, set()) + other.originals.get(key, set())
+                            for key in set(self.originals.keys() + other.originals.keys())}
+
+    def __str__(self):
+        """Return a string representation"""
+        mc = self.most_common()
+        return '\n'.join(('{} [{}]:\n\t'.format(baseform, count) + \
+            ', '.join((pa.word for pa in self.originals[baseform])) \
+            for baseform, count in self.most_common()))
+
+
+class AnagramImporter(object):
+    """Implements import mechanics for text files full of potential anagrams"""
+
+    def __init__(self, extension='txt'):
+        self.extension = extension
+
+    def find_module(self, fullname, path=None):
+        """Locate a matching module"""
+        for dirname in sys.path:
+            filename = os.path.join(dirname, '.'.join((fullname, self.extension)))
+            if os.path.isfile(filename):
+                return AnagramLoader(filename, self.extension)
+        return None
+
+
+class AnagramLoader(object):
+    """Loads a text file of anagrams into a Python object"""
+
+    def __init__(self, filename, extension):
+        self.filename = filename
+        self.extension = extension
+
+    def load_module(self, fullname):
+        """Create a new modules based on the contents of filename"""
+        module = sys.modules.setdefault(fullname, imp.new_module(fullname))
+        module.__file__ = self.filename
+        module.__loader__ = self
+        try:
+            with open(self.filename) as fp:
+                lines = (line.strip() for line in fp.readlines())
+        except UnicodeDecodeError:
+            with open(self.filename, encoding='latin-1') as fp:
+                lines = (line.strip() for line in fp.readlines())
+        obj = Tracker()
+        obj(lines)
+        setattr(module, self.extension, obj)
+        return module
+
+
+sys.meta_path.append(AnagramImporter())
---
+++ go.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+import anagram
+import wordlist
+print(wordlist.txt)
